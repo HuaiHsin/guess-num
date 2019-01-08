3e91d996bfc02abfb8176a17b39b1f41ed1a72f8
HuaiHsin <huaihsin@gmail.com>
2019-01-08 23:37:56 +0800

    become guess num game

--- guess-num.py
+++ guess-num.py
@@ -8,7 +8,9 @@ num = random.randint(int(min_num), int(max_num))
 
 while True:
     guess_num = int(input("請輸入"+ min_num+ "~"+ max_num+ "之間的數字： "))
-    if guess_num > num:
+    if guess_num > int(max_num) or guess_num < int(min_num):
+        print("超出數字範圍囉～數字介於",min_num,"和",max_num,"之間")
+    elif guess_num > num:
         print("猜錯囉！你的數字比答案大")
         times+=1
     elif guess_num < num:
@@ -17,5 +19,5 @@ while True:
     else:
         print("終於猜對了！")
         times+=1
-        print("你總共猜了" + str(times) + " 次呢")
+        print("你總共猜了", times, " 次呢")
         break
